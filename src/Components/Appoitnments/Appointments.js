4ca7ce647eb8220c75835e5867bda69c938506e2
Rahul Paul <rahulshanto008@gmail.com>
2022-05-05 01:53:13 +0600

    appointment route done

--- src/Components/Appoitnments/Appointments.js
+++ src/Components/Appoitnments/Appointments.js
@@ -1,64 +1,66 @@
 import React from 'react';
-import { Button, Col, Form, Row } from 'react-bootstrap';
+import { Button, Col, Form, } from 'react-bootstrap';
 import './Appointments.css';
+import doctor from '../../images/doctor-.png'
 
 const Appointments = () => {
     return (
-        <div>
+        <div className='apointment'>
+            <img style={{ height: '500px', width: '500px' }} src={doctor} alt="" />
 
-            <Form>
-                <Row className="mb-3">
-                    <Form.Group as={Col} controlId="formGridState">
-                        <Form.Label>Select Department</Form.Label>
-                        <Form.Select defaultValue="Choose...">
-                            <option>Choose...</option>
-                            <option>Gynaecology</option>
-                            <option>Cardiology</option>
-                            <option>Orthopaedics</option>
-                            <option>Medicine</option>
-                        </Form.Select>
-                    </Form.Group>
-                    <Form.Group as={Col} controlId="formGridState">
-                        <Form.Label>Choose Doctor By Name</Form.Label>
-                        <Form.Select defaultValue="Choose...">
-                            <option>Choose...</option>
-                            <option>Naidan Smith</option>
-                            <option>Danial Frankie</option>
-                            <option>Jason Roy</option>
-                            <option>Alexa John</option>
-                        </Form.Select>
-                    </Form.Group>
-                    <Form.Group as={Col} controlId="formGridEmail">
-                        <Form.Label>Write Your Name</Form.Label>
-                        <Form.Control type="name" placeholder="Patient Name" />
-                    </Form.Group>
-                    <br />
-                    <br />
-                    <Form.Group className='form-box' controlId="formGridAddress1">
-                        <Form.Label>Phone Number</Form.Label>
-                        <Form.Control type='text' placeholder="Patient Number" />
-                    </Form.Group>
-                    <br />
+            <Form className='Form'>
 
-                    <Form.Group className='form-box' controlId="formGridAddress2">
-                        <Form.Label>Your Email</Form.Label>
-                        <Form.Control type="email" placeholder="example@mail.com" />
-                    </Form.Group>
-                    <Form.Group controlId="formGridAddress2">
-                        <Form.Label></Form.Label>
-                        <Form.Control type="text" placeholder="Type Appointment Note" />
-                    </Form.Group>
+                <Form.Group className='form-box' as={Col} controlId="formGridState">
+                    <Form.Label>Select Department</Form.Label>
+                    <Form.Select defaultValue="Choose...">
+                        <option>Choose...</option>
+                        <option>Gynaecology</option>
+                        <option>Cardiology</option>
+                        <option>Orthopaedics</option>
+                        <option>Medicine</option>
+                    </Form.Select>
+                </Form.Group>
+                <br />
 
-                    <Form.Group id="formGridCheckbox">
-                        <Form.Check type="checkbox" label="Old Patient?" />
-                    </Form.Group>
-
-                    <Button variant="primary" type="submit">
-                        Submit
-                    </Button>
+                <Form.Group className='form-box' as={Col} controlId="formGridState">
+                    <Form.Label>Choose Doctor By Name</Form.Label>
+                    <Form.Select defaultValue="Choose...">
+                        <option>Choose...</option>
+                        <option>Naidan Smith</option>
+                        <option>Danial Frankie</option>
+                        <option>Jason Roy</option>
+                        <option>Alexa John</option>
+                    </Form.Select>
+                </Form.Group>
 
 
-                </Row>
+
+                <Form.Group as={Col} className="form-box" controlId="formGridEmail">
+                    <Form.Label>Write Your Name</Form.Label>
+                    <Form.Control type="name" placeholder="Patient Name" />
+                </Form.Group>
+                <Form.Group className='form-box' controlId="formGridAddress1">
+                    <Form.Label>Phone Number</Form.Label>
+                    <Form.Control type='text' placeholder="Patient Number" />
+                </Form.Group>
+
+
+                <Form.Group className='form-box' controlId="formGridAddress2">
+                    <Form.Label>Your Email</Form.Label>
+                    <Form.Control type="email" placeholder="example@mail.com" />
+                </Form.Group>
+                <br />
+                <br />
+                <Form.Group controlId="formGridAddress2">
+
+                    <Form.Control className='appointment-textarea' type="text" placeholder="Type Appointment Note" />
+                </Form.Group>
+
+
+
+                <Button className='submit-box' variant="primary" type="submit">
+                    Submit
+                </Button>
 
 
             </Form>
--- src/Shared/Header/Header.js
+++ src/Shared/Header/Header.js
@@ -1,7 +1,5 @@
 import React from 'react';
 import { Container, Nav, Navbar, } from 'react-bootstrap';
-import { HashLink } from 'react-router-hash-link';
-import { Link } from 'react-router-dom';
 import logo from "../../images/navbar.jpg";
 
 import './Header.css'
